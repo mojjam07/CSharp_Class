e12ad0c76e391b4157c67b3a5b720ffcff8489e5
mojjam07 <mojjam07@gmail.com>
2025-10-22 16:43:02 +0100

    my c# day 1 class

---
+++ Program.cs
@@ -0,0 +1,14 @@
+﻿// See https://aka.ms/new-console-template for more information
+Console.WriteLine("Hello, World!");
+
+// Variables and Data Types
+int age = 20;
+string name = "Mojeed";
+Console.WriteLine($"My name is {name} and I am {age} years old.");
+
+// Taking User Inputs
+Console.Write("Enter your name: ");
+string userName = Console.ReadLine();
+Console.Write("Enter your age: ");
+int userAge = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine($"Hello, {userName}! You are {userAge} years old.");
