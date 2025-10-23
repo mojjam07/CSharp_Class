71638152202b551e82467687b2123b976da5113b
mojjam07 <mojjam07@gmail.com>
2025-10-23 07:58:59 +0100

    assignment done

--- Program.cs
+++ Program.cs
@@ -12,3 +12,23 @@ string userName = Console.ReadLine();
 Console.Write("Enter your age: ");
 int userAge = Convert.ToInt32(Console.ReadLine());
 Console.WriteLine($"Hello, {userName}! You are {userAge} years old.");
+
+// Assignment 1
+Console.Write("Enter your name: ");
+string myname = Console.ReadLine();
+Console.Write("Enter your age: ");
+int myage = Convert.ToInt32(Console.ReadLine());
+Console.Write("What is your hobby: ");
+string hobby = Console.ReadLine();
+Console.WriteLine($"Hello, {myname}! You are {myage} years old and your hobby is {hobby}.");
+
+int num1 = 10;
+int num2 = 15;
+int sum = num1 + num2;
+Console.WriteLine($"The sum of {num1} and {num2} is {sum}.");
+
+Console.Write("What is your name: ");
+string yourname = Console.ReadLine();
+Console.Write("What is your favorite food: ");
+string food = Console.ReadLine();
+Console.WriteLine($"Hello, {yourname}! Your favorite food is {food}.");
--- obj/Debug/net8.0/DotNet.AssemblyInfo.cs
+++ obj/Debug/net8.0/DotNet.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("DotNet")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+e12ad0c76e391b4157c67b3a5b720ffcff8489e5")]
 [assembly: System.Reflection.AssemblyProductAttribute("DotNet")]
 [assembly: System.Reflection.AssemblyTitleAttribute("DotNet")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
